a0b25d23250b22084752845609ba7ab9511141ec
Muskan2217 <82403062+Muskan2217@users.noreply.github.com>
2021-09-14 21:09:43 +0530

    final project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function bulbOn() {
+  let bulbobj = document.querySelector(".bulbOff");
+  bulbobj.setAttribute("src", "imgs/bulb-On.png");
+}
+
+function bulbOff() {
+  let bulbobj = document.querySelector(".bulbOff");
+  bulbobj.setAttribute("src", "imgs/bulb-Off.png");
+}
