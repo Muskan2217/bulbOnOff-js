b6edcbf984f223a717034fc1d4144abb2e44dda1
Muskan2217 <82403062+Muskan2217@users.noreply.github.com>
2021-09-17 18:36:03 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ function bulbOff() {
   let bulbobj = document.querySelector(".bulbOff");
   bulbobj.setAttribute("src", "imgs/bulb-Off.png");
 }
+
+document.getElementById("onbtn").addEventListener("click", function () {
+  document.getElementById("cont").style.boxShadow =
+    "#fff71ac6 0px 22px 70px 4px";
+});
+
+document.getElementById("offbtn").addEventListener("click", function () {
+  document.getElementById("cont").style.boxShadow = "none";
+});
